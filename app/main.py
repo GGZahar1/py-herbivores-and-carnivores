8bea2b0321da2281f01102678409e79915c0a2da
Zahar <galushkozahar>
2025-01-25 19:36:13 +0200

    Виправлення помилок згідно з flake8

--- app/main.py
+++ app/main.py
@@ -1,31 +1,46 @@
 # write your code here
 from __future__ import annotations
+
 class Animal:
+
+
     alive = []
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+
+    def __init__(self, name: str,
+                 health: int = 100, hidden:
+                 bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
+
     def __repr__(self):
+
         return (
             f"{{Name: {self.name}"
             f", Health: {self.health}"
             f", Hidden: {self.hidden}}}"
         )
+
     @classmethod
     def remove(cls) -> None:
+
+
         cls.alive = [animal for animal in cls.alive if animal.health >= 0]
+
 
     def die(self) -> None:
         self.health = 0
         Animal.remove()
 
 class Herbivore(Animal):
+
     def hide(self) -> None:
         self.hidden = not self.hidden
 
 class Carnivore(Animal):
+
+
     def bite(self, animal: Animal) -> None:
         if isinstance(animal, Herbivore) and animal.health > 0 and not animal.hidden:
             animal.health -= 50
